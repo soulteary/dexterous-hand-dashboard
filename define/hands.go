a8a88214893ec60b6daa142ba69093057a6b591d
Eli Yip <yezi.2022@outlook.com>
2025-05-28 09:45:01 +0800

    chore: add String method for hand type

--- define/hands.go
+++ define/hands.go
@@ -6,3 +6,10 @@ const (
 	HAND_TYPE_LEFT  HandType = 0x28
 	HAND_TYPE_RIGHT HandType = 0x27
 )
+
+func (ht HandType) String() string {
+	if ht == HAND_TYPE_LEFT {
+		return "左手"
+	}
+	return "右手"
+}
